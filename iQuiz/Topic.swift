a3923108b1cfe99212a3495bd8efc434903b44ea
Nattanon Bunyatipanon <nattanon33@yahoo.com>
2018-10-28 00:50:07 -0700

    finish homework with 4 cells and auto layout

---
+++ iQuiz/Topic.swift
@@ -0,0 +1,25 @@
+//
+//  Topic.swift
+//  iQuiz
+//
+//  Created by Nattanon Bunyatipanon on 10/27/18.
+//  Copyright © 2018 Nattanon Bunyatipanon. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Topic {
+    
+    var image: UIImage
+    var title: String
+    var description: String
+    
+    init(image: UIImage, title: String, description: String) {
+        self.image = image
+        self.title = title
+        self.description = description
+        
+    }
+    
+}
--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -10,11 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tblTableView: UITableView!
+    
+    var topicCells: [Topic] = []
+    var images: [UIImage] = [#imageLiteral(resourceName: "laf"),#imageLiteral(resourceName: "food"),#imageLiteral(resourceName: "games"),#imageLiteral(resourceName: "songs")]
+    var topics: [String] = ["Cars", "Food", "Games", "Songs"]
+    var descriptions: [String] = ["These quizzes will be all about cars!", "These quizzes will be all about food!", "These quizzes will be all about games!", "These quizzes will be all about songs!"]
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.topicCells = createArrays()
+        tblTableView.delegate = self
+        tblTableView.dataSource = self
     }
-
-
+    
+    
+    
+    // Settings Button Function for alert popups
+    @IBAction func btnOnSettingsTap(_ sender: Any) {
+        // Creating Alert popup
+        let alert = UIAlertController(title: "Settings", message: "Check back for settings!", preferredStyle: .alert)
+        
+        // Creating the "OK" action button
+        let actionOK = UIAlertAction(title: "Ok", style: .default) {
+            (action) in
+            //print("Test Action")
+        }
+        
+        // Add action button to alert popup
+        alert.addAction(actionOK)
+        
+        // Presenting it to user
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    func createArrays() -> [Topic] {
+        var tempTopics: [Topic] = []
+        
+        for i in 0...self.images.count - 1 {
+            tempTopics.append(Topic(image: self.images[i], title: self.topics[i], description: self.descriptions[i]))
+        }
+        
+        return tempTopics
+    }
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    // Returns the number of rows we have
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.topicCells.count
+    }
+    
+    // Configuring every cell as it appears
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let info = self.topicCells[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TopicCell") as! TopicCell
+        
+        cell.setTopicCell(topic: info)
+        return cell
+    }
+}
