2415b93336e1a54b6cdb218c80c92e240cbe43da
Nattanon Bunyatipanon <nattanon33@yahoo.com>
2018-11-13 22:57:04 -0800

    done

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -8,102 +8,165 @@
 
 import UIKit
 
-
-
-class ViewController: UIViewController {
-
-    @IBOutlet weak var tblTableView: UITableView!
-    
-    var topicCells: [Topic] = []
-    var images: [UIImage] = [#imageLiteral(resourceName: "laf"),#imageLiteral(resourceName: "food"),#imageLiteral(resourceName: "games"),#imageLiteral(resourceName: "songs")]
-    var topics: [String] = ["Cars", "Food", "Games", "Songs"]
-    var descriptions: [String] = ["These quizzes will be all about cars!", "These quizzes will be all about food!", "These quizzes will be all about games!", "These quizzes will be all about songs!"]
-    var questions: [String : [Question]] =
-        [   "Cars":
-                [Question(question: "What is the fastest 0-60 MPH production car as of November 2018?", choices: ["Porsche 918","Ferrari LaFerrari","Lamborghini Huracan Performante","Tesla Roadster"], correctChoice: 3),
-                 Question(question: "What is Lamborghini's SUV model called?", choices: ["Aventador", "Focus", "Urus", "Prius"], correctChoice: 2)
-                ],
-            "Food":
-                [Question(question: "Where does curry originate from?", choices: ["India","Thailand","Malaysia","China"], correctChoice: 0),
-                 Question(question: "Which fruit has the highest protein content?", choices: ["Bananas","Avocado","Durian","Grapefruit"], correctChoice: 1)
-            ],
-            "Games":
-                [Question(question: "Which real-world animal inspired the creation of the Pokemon series?", choices: ["Butterflies","Lynx","Turtles","Chickens"], correctChoice: 0),
-                 Question(question: "What is the highest grossing video game of all time?", choices: ["League of Legends", "Counter Strike - Global Offensive", "Mario Kart Double Dash", "World of Warcraft"], correctChoice: 3)
-            ],
-            "Songs":
-            [Question(question: "Which music video is the most viewed on YouTube of all time?", choices: ["Psy - Gangnam Style","Taylor Swift - Shake It Off","Wiz Khalifa - See You Again","Justin Bieber - Sorry"], correctChoice: 2),
-            Question(question: "In what year was Vanilla Ice's \"Ice Ice Baby\" realeased?", choices: ["1991", "1990", "1995", "2000"], correctChoice: 1)
-            ]
-        ]
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.topicCells = createArrays()
-        tblTableView.delegate = self
-        tblTableView.dataSource = self
-    }
-    
-    
-    // Settings Button Function for alert popups
-    @IBAction func btnOnSettingsTap(_ sender: Any) {
-        // Creating Alert popup
-        let alert = UIAlertController(title: "Settings", message: "Check back for settings!", preferredStyle: .alert)
-        
-        // Creating the "OK" action button
-        let actionOK = UIAlertAction(title: "Ok", style: .default) {
-            (action) in
-            //print("Test Action")
-        }
-        
-        // Add action button to alert popup
-        alert.addAction(actionOK)
-        
-        // Presenting it to user
-        present(alert, animated: true, completion: nil)
-    }
-    
-    
-    func createArrays() -> [Topic] {
-        var tempTopics: [Topic] = []
-        
-        for i in 0...self.images.count - 1 {
-            tempTopics.append(Topic(image: self.images[i], title: self.topics[i], description: self.descriptions[i], questions: self.questions[self.topics[i]]!))
-        }
-        
-        return tempTopics
-    }
+struct Quiz: Codable{
+    let title: String
+    let desc: String
+    let questions: [Question]
 }
 
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
+struct Question: Codable{
+    let text: String
+    let answer: String
+    let answers: [String]
+}
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    // Returns the number of rows we have
+    @IBOutlet weak var tableView: UITableView!
+    let defaults = UserDefaults.standard
+    var toStore: NSArray = []
+    var jsonData: [Quiz]? = nil
+    var elements: [String] = ["science", "marvel", "math"]
+    var titles: [String] = []
+    var descriptions: [String] = []
+    var jsonUrlString: String = UserDefaults.standard.string(forKey: "url") ?? "http://tednewardsandbox.site44.com/questions.json"
+    
+    //Test URL: https://raw.githubusercontent.com/Nonj/iQuiz/partThree/moreQuiz.json
+    
+    var urlTextField: UITextField = UITextField()
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.topicCells.count
+        return jsonData?.count ?? 0;
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 125;
     }
     
-    
-    // Configuring every cell as it appears
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let info = self.topicCells[indexPath.row]
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TopicCell") as! TopicCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customCell") as! CustomTableViewCell
+        cell.descriptionLabel.text = jsonData?[indexPath.row].desc
+        cell.cellLabel.text = jsonData?[indexPath.row].title
         
-        cell.setTopicCell(topic: info)
+        cell.cellImage.image = UIImage(named: "default")
+        //check if title has icon if not use default.
+        for  i  in 0...elements.count-1{
+            if((cell.cellLabel.text?.lowercased().range(of: elements[i])) != nil){
+                cell.cellImage.image = UIImage(named: elements[i])
+            }
+        }
+        
         return cell
     }
     
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "toQuestion", sender: self)
+    @IBAction func settingsPressed(_ sender: Any) {
+        let alert = UIAlertController(title: "Settings", message: "Enter Data URL", preferredStyle: UIAlertControllerStyle.alert)
+        
+        alert.addTextField { (textField: UITextField) in
+            self.urlTextField = textField
+            self.urlTextField.placeholder = "Enter url here"
+        }
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.default, handler: nil))
+        alert.addAction(UIAlertAction(title: "Check now", style: UIAlertActionStyle.default, handler:{
+            (act: UIAlertAction) in
+            if((self.urlTextField.text) != nil){
+                self.fetchJson(self.urlTextField.text!)
+            }
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
+        
+        //grabbing url fetched successfully last time
+        if((defaults.object(forKey: "url")) != nil){
+            self.jsonUrlString = defaults.object(forKey: "url") as! String
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if(Reachability.isConnectedToNetwork()){
+            if(jsonData == nil){
+                fetchJson(jsonUrlString)
+            }
+        }else{
+            let alert = UIAlertController(title: "No Internet", message: "Using local data", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            print("no internet, grabbing from user default");
+            let anyData = defaults.object(forKey: "quizData")
+            if(anyData == nil){
+                let alert = UIAlertController(title: "No Local Data", message: "Please connect to internet", preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }else{
+                let data = anyData as! Data
+                do{
+                    let questions = try JSONDecoder().decode([Quiz].self, from: data)
+                    self.jsonData = questions
+                    for q in questions{
+                        self.titles.append(q.title)
+                        self.descriptions.append(q.desc)
+                    }
+                }catch{
+                    self.failDownloadAlert()
+                }
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let destination = segue.destination as? QuestionController {
-            destination.fromTopic = self.topicCells[(tblTableView.indexPathForSelectedRow?.row)!]
-            
+        //check which cell is pressed, and send over data
+        if let indexPath = tableView.indexPathForSelectedRow{
+            let categoryIndex = indexPath.row
+            let questionView = segue.destination as! QuestionViewController
+            questionView.jsonData = jsonData
+            questionView.categoryIndex = categoryIndex
         }
+    }
+    
+    func fetchJson(_ fetchUrl: String){
+        
+        guard let url = URL(string: fetchUrl) else {return}
+        URLSession.shared.dataTask(with: url) { (data, res, err) in
+            guard let data = data else {
+                self.failDownloadAlert()
+                return
+            }
+            self.defaults.set(fetchUrl, forKey: "url")
+            self.defaults.set(data, forKey:"quizData")
+            do{
+                let questions = try JSONDecoder().decode([Quiz].self, from: data)
+                self.jsonData = questions
+                for q in questions{
+                    self.titles.append(q.title)
+                    self.descriptions.append(q.desc)
+                }
+            }catch{
+                self.failDownloadAlert()
+            }
+            
+            DispatchQueue.main.async{
+                self.tableView.reloadData()
+            }
+            }.resume()
+    }
+    
+    func failDownloadAlert(){
+        let alert = UIAlertController(title: "Download Failed", message: "Please check internet/ data URL/ data format", preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
 }
